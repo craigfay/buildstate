70f714ba116c0dea16b049c11566187dff34caa3
Craig Fay <craigfay1@gmail.com>
2019-09-25 20:30:45 -0500

    Fullfull interface

--- app/services/history.ts
+++ app/services/history.ts
@@ -11,40 +11,46 @@ const dir = promisify(fs.readdir);
 
 const mutationsDir = '/Users/craigfay/Repositories/warehouse/app/mutations';
 
+function history(mutationPath): Rebuildable {
+  return {
+    async rebuild() {
+      let data:any = {};
+      const id = () => randomBytes(32).toString('hex');
+    
+      const mutationFiles = await dir(mutationPath);
+      for (const file of mutationFiles) {
+        const mutater = require(`${mutationPath}/${file}`);
+        data = mutater(data, id);
+      }
+      return data;
+    },
+
+    async commit(fn) {
+      const timestamp = new Date().toISOString();
+      const code = `module.exports = ${fn.toString()}`;
+      const contents = Uglify.minify(code).code;
+      await write(`${mutationsDir}/${timestamp}.js`, contents);
+      return true;
+    }
+  }
+}
+
 type Commitable = (data:any, id:() => string) => any;
 export interface Rebuildable {
-  rebuild: (buildfile:string) => object
-  commit: (commit:Commitable) => boolean
+  rebuild: () => Promise<any>
+  commit: (commit:Commitable) => Promise<boolean>
 }
 
-async function commit(fn) {
-  const timestamp = new Date().toISOString();
-  const code = `module.exports = ${fn.toString()}`;
-  const contents = Uglify.minify(code).code;
-  await write(`${mutationsDir}/${timestamp}.js`, contents);
-}
-
-async function rebuild() {
-  let data:any = {};
-  const id = () => randomBytes(32).toString('hex');
-
-  const mutationFiles = await dir(mutationsDir);
-  for (const file of mutationFiles) {
-    const mutater = require(`${mutationsDir}/${file}`);
-    data = mutater(data, id);
-  }
-  return data;
-}
-
-function seed() {
-  commit((data, id) => {data.products = []; return data;});
-  commit((data, id) => {data.products.push({ id: id(), name: 'Lumbar Pillow', price: 2000 }); return data;});
-  commit((data, id) => {data.products.push({ id: id(), name: 'Soft Rug', price: 3000 }); return data;});
-  commit((data, id) => {data.products.push({ id: id(), name: 'Comfy Blanket', price: 4000 }); return data;});
+async function seed() {
+  const h = history(mutationsDir);
+  h.commit((data, id) => {data.products = []; return data;});
+  h.commit((data, id) => {data.products.push({ id: id(), name: 'Lumbar Pillow', price: 2000 }); return data;});
+  h.commit((data, id) => {data.products.push({ id: id(), name: 'Soft Rug', price: 3000 }); return data;});
+  h.commit((data, id) => {data.products.push({ id: id(), name: 'Comfy Blanket', price: 4000 }); return data;});
+  console.log(await h.rebuild())
 }
 
 
 (async function main() {
-  // const data = await rebuild();
-  // console.log(JSON.stringify(data, null, 2));
+  seed();
 })()
